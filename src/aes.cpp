0655508af684b1d34820b1d4783c6a973da709d9
Aster Seker <git.electroyar@gmail.com>
2025-09-06 06:51:34 +0300

    fix(build): restrict cpuid include to x86

--- src/aes.cpp
+++ src/aes.cpp
@@ -34,7 +34,9 @@
 #include <immintrin.h>
 #include <intrin.h>
 #elif defined(__has_include)
-#if __has_include(<cpuid.h>)
+#if (defined(__x86_64__) || defined(_M_X64) || defined(__i386) || \
+     defined(_M_IX86)) &&                                         \
+    __has_include(<cpuid.h>)
 #include <cpuid.h>
 #endif
 #endif
